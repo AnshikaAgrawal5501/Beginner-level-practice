0abc30f9d6b836aca6ef0290138cd23c7336fa6a
Anshika Agrawal <anshikaagrawal5501@gmail.com>
2020-07-09 22:46:25 +0530

    adding cryptography

---
+++ beginner/cryptography.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class cryptography here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class cryptography
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class cryptography
+     */
+    public cryptography()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
